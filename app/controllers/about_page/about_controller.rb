658848dee3ae40ed3a2ebd833b08dedb6bbd5b06
Michael Klein <mbklein@gmail.com>
2012-04-19 18:10:13 -0700

    Render dependency tree as a series of nested <ul>s

--- app/controllers/about_page/about_controller.rb
+++ app/controllers/about_page/about_controller.rb
@@ -21,12 +21,11 @@ module AboutPage
           end
         end
       end
-    
-      @dependencies = Bundler.load.specs.sort_by { |s| s.name }.collect do |spec|
-        [spec.name,[spec.version,spec.git_version].join(" ")]
-      end
-      @dependencies.unshift([Configuration.app_name,Configuration.app_version])
-
+      
+      env = Bundler.load
+      groups = env.current_dependencies.group_by { |d| d.groups.first.to_s }
+      @dependencies = Hash[groups.collect { |group,deps| [group,Hash[deps.collect { |dep| [dep.name,dependency_hash(env, dep.name)] }]] }]
+      
       @solr_profile = Configuration.solr.nil? ? {} : Configuration.solr.luke(:show => 'schema', :numTerms => 0)['index']
       @fedora_profile = Configuration.fedora.nil? ? {} : Configuration.fedora.profile
       
@@ -37,5 +36,23 @@ module AboutPage
       end
     end
 
+    private
+    def dependency_version(env, key)
+      spec = env.specs.find { |s| s.name == key }
+      rev = spec.git_version
+      rev.strip! unless rev.nil?
+      location = [spec.source.options.values_at('path','uri').compact.first,rev].compact.join('@')
+      [spec.version.to_s,location].compact.join(' ').strip
+    end
+    
+    def dependency_hash(env, key, graph=nil)
+      graph = Bundler::Graph.new(env,'/dev/null') if graph.nil?
+      result = { :version => dependency_version(env, key) }
+      deps = Hash[graph.relations[key].collect { |dep| [dep, dependency_hash(env, dep, graph)]}]
+      result[:dependencies] = deps unless deps.empty?
+      result
+    end
+    
+    
   end
 end
