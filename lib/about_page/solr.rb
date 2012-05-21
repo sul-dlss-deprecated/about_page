6eaa5ad36d287e06f14fa41fafdaadca1ccb8716
Chris Beer <chris@cbeer.info>
2012-05-21 12:48:38 -0700

    prototyping solr ok/set_header logic

--- lib/about_page/solr.rb
+++ lib/about_page/solr.rb
@@ -2,14 +2,41 @@ module AboutPage
   class Solr  < AboutPage::Configuration::Node
     delegate :each_pair, :to_json, :to_xml, :to => :to_h
 
-    attr_accessible :rsolr
+    attr_accessor :rsolr, :options
 
-    def initialize rsolr_instance
+    def initialize rsolr_instance, options = {}
       self.rsolr = rsolr_instance
+      self.options = options
+
+      self.options[:minimum_numdocs] ||= 1
+    end
+
+    def schema
+      @schema ||= rsolr.luke(:show => 'schema', :numTerms => 0)
+    rescue
+      {}
+    end
+
+    def index
+      schema['index'] || {}
     end
 
     def to_h
-      rsolr.luke(:show => 'schema', :numTerms => 0)['index']
+      index
+    end
+
+    def ok?
+      true if index[:numDocs].to_i >= self.options[:minimum_numdocs]
+    rescue
+      false
+    end
+
+    def set_headers! response
+      add_header(response, "solr numDocs: #{index[:numDocs]} < #{self.options[:minimum_numdocs]}") if index[:numDocs].to_i < self.options[:minimum_numdocs]
+    end
+
+    def preflight request
+      self.options[:minimum_numdocs] = request.params['solr.numDocs'].to_i if request.params['solr.numDocs'].to_i
     end
   end
 end
