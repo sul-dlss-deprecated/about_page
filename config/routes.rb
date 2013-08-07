02d339df1e6e64253b51a3da69e8d1416df4c164
Michael Klein <mbklein@gmail.com>
2013-08-06 23:56:32 -0500

    Make it pretty

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 AboutPage::Engine.routes.draw do
   match 'health' => 'about_page/about#health'
-  match ":filter" => 'about_page/about#index'
+  match ":filter" => 'about_page/about#index', :as => 'component'
   root :to => 'about_page/about#index'
 end
--- lib/about_page/version.rb
+++ lib/about_page/version.rb
@@ -1,3 +1,3 @@
 module AboutPage
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
 end
