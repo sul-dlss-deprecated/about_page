731a84364fcd9c28124a144aa9cbb5a969cb4ab9
Brian Keese <bkeese@indiana.edu>
2014-05-07 16:37:23 -0400

    for rails4 added via: to matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 AboutPage::Engine.routes.draw do
-  match 'health' => 'about_page/about#health'
-  match ":filter" => 'about_page/about#index', :as => 'component'
+  match 'health' => 'about_page/about#health', via: :all
+  match ":filter" => 'about_page/about#index', :as => 'component', via: :all
   root :to => 'about_page/about#index'
 end
