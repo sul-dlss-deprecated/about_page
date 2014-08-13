b9c0582fac09f0a8c0a7abc3d0c8bf3c9dd87807
Michael Klein <mbklein@gmail.com>
2014-08-13 13:32:20 -0500

    Don't cache commits

--- lib/about_page/git_log.rb
+++ lib/about_page/git_log.rb
@@ -6,23 +6,26 @@ module AboutPage
     
     def initialize(opts = {})
       root  = opts.fetch(:root, Rails.root)
-      limit = opts.fetch(:limit, %r{/tags/})
-      repo = Grit::Repo.new(root)
-      refs = repo.refs_list.inject(Hash.new { |h,k| h[k] = [] }) { |h,r| h[r[1]] << r[0]; h }
-      @commits = repo.log(repo.head.name).inject([]) do |a,c|
+      @limit = opts.fetch(:limit, %r{/tags/})
+      @repo = Grit::Repo.new(root)
+    end
+    
+    def commits
+      refs = @repo.refs_list.inject(Hash.new { |h,k| h[k] = [] }) { |h,r| h[r[1]] << r[0]; h }
+      @repo.log(@repo.head.name).inject([]) do |a,c|
         a << { :commit => c.to_hash, :refs => refs[c.sha] }
-        bail = case limit
-        when Fixnum then a.length == limit
-        when Regexp then a.last[:refs].any? { |ref| ref =~ limit }
-        when Time   then c.committed_date < limit
-        when Proc   then limit.call(c)
+        bail = case @limit
+        when Fixnum then a.length == @limit
+        when Regexp then a.last[:refs].any? { |ref| ref =~ @limit }
+        when Time   then c.committed_date < @limit
+        when Proc   then @limit.call(c)
         end
         if bail then break(a) else a end
       end
     end
     
     def to_h
-      @commits
+      commits
     end
   end
 end
