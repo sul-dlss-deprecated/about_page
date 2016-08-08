42ec4aa5a16d626280291a931740ad74fa1c8a9a
Michael B. Klein <mbklein@gmail.com>
2016-08-08 14:26:05 -0500

    Make rugged an optional dependency and soldier on without it

--- lib/about_page/git_log.rb
+++ lib/about_page/git_log.rb
@@ -1,18 +1,23 @@
-require 'rugged'
+begin
+  require 'rugged'
+rescue LoadError
+end
 
 module AboutPage
   class GitLog  < AboutPage::Configuration::Node
     attr_reader :commits
     
     def initialize(opts = {})
-      root  = opts.fetch(:root, Rails.root)
-      @limit = opts.fetch(:limit, %r{/tags/})
-      @repo = Rugged::Repository.new(root.to_s)
-      @fetch = !!opts[:fetch]
-      if opts[:remotes]
-        opts[:remotes].each_pair do |name, location|
-          if @repo.remotes[name.to_s].nil?
-            @repo.remotes.create(name.to_s, location)
+      if defined?(Rugged)
+        root  = opts.fetch(:root, Rails.root)
+        @limit = opts.fetch(:limit, %r{/tags/})
+        @repo = Rugged::Repository.new(root.to_s)
+        @fetch = !!opts[:fetch]
+        if opts[:remotes]
+          opts[:remotes].each_pair do |name, location|
+            if @repo.remotes[name.to_s].nil?
+              @repo.remotes.create(name.to_s, location)
+            end
           end
         end
       end
@@ -27,18 +32,22 @@ module AboutPage
     end
     
     def commits
-      @repo.remotes.each { |remote| remote.fetch rescue nil } if @fetch
-      refs = @repo.refs.inject(Hash.new { |h,k| h[k] = [] }) { |h,r| h[find_commit(r).oid] << r.name; h }
       result = []
-      @repo.walk(@repo.head.name, Rugged::SORT_TOPO+Rugged::SORT_DATE) do |c|
-        result << { :commit => c, :refs => refs[c.oid] }
-        bail = case @limit
-        when Fixnum then result.length == @limit
-        when Regexp then result.last[:refs].any? { |ref| ref =~ @limit }
-        when Time   then c.committer[:time] < @limit
-        when Proc   then @limit.call(c)
+      if @repo.nil?
+        Rails.logger.warn 'rugged gem not found.'
+      else
+        @repo.remotes.each { |remote| remote.fetch rescue nil } if @fetch
+        refs = @repo.refs.inject(Hash.new { |h,k| h[k] = [] }) { |h,r| h[find_commit(r).oid] << r.name; h }
+        @repo.walk(@repo.head.name, Rugged::SORT_TOPO+Rugged::SORT_DATE) do |c|
+          result << { :commit => c, :refs => refs[c.oid] }
+          bail = case @limit
+          when Fixnum then result.length == @limit
+          when Regexp then result.last[:refs].any? { |ref| ref =~ @limit }
+          when Time   then c.committer[:time] < @limit
+          when Proc   then @limit.call(c)
+          end
+          break if bail
         end
-        break if bail
       end
       result
     end
